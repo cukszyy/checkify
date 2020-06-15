4315ace3731b6371aa5495c6bdd8502ca53f04a8
Lucas Portella <lportella@Lucass-MacBook-Pro.local>
2020-06-14 23:39:10 -0300

    Add checklist question screen

---
+++ lib/utilities/checklist/question.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:checkify/utilities/checklist/dot.dart';
+import 'package:checkify/utilities/checklist/step_question.dart';
+import 'package:checkify/utilities/checklist/step_number.dart';
+import 'package:checkify/utilities/checklist/option_item.dart';
+
+class Question extends StatefulWidget {
+  final int number;
+  final String question;
+  final List<String> answers;
+  final VoidCallback onOptionSelected;
+
+  const Question({
+    Key key,
+    @required this.onOptionSelected,
+    @required this.number,
+    @required this.question,
+    @required this.answers,
+  }) : super(key: key);
+
+  @override
+  _QuestionState createState() => _QuestionState();
+}
+
+class _QuestionState extends State<Question>
+    with SingleTickerProviderStateMixin {
+  List<GlobalKey<_ItemFaderState>> keys;
+  int selectedOptionKeyIndex;
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    keys = List.generate(
+      2 + widget.answers.length,
+      (_) => GlobalKey<_ItemFaderState>(),
+    );
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    onInit();
+  }
+
+  void onInit() async {
+    for (GlobalKey<_ItemFaderState> key in keys) {
+      await Future.delayed(Duration(milliseconds: 40));
+      key.currentState.show();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 32),
+        ItemFader(
+          key: keys[0],
+          child: StepNumber(number: widget.number),
+        ),
+        ItemFader(
+          key: keys[1],
+          child: StepQuestion(question: widget.question),
+        ),
+        Spacer(),
+        ...widget.answers.map((String answer) {
+          int answerIndex = widget.answers.indexOf(answer);
+          int keyIndex = answerIndex + 2;
+          return ItemFader(
+            key: keys[keyIndex],
+            child: OptionItem(
+              name: answer,
+              onTap: (offset) => onTap(keyIndex, offset),
+              showDot: selectedOptionKeyIndex != keyIndex,
+            ),
+          );
+        }),
+        SizedBox(height: 64),
+      ],
+    );
+  }
+
+  void onTap(int keyIndex, Offset offset) async {
+    for (GlobalKey<_ItemFaderState> key in keys) {
+      await Future.delayed(Duration(milliseconds: 40));
+      key.currentState.hide();
+      if (keys.indexOf(key) == keyIndex) {
+        setState(() => selectedOptionKeyIndex = keyIndex);
+        animateDot(offset).then((_) => widget.onOptionSelected());
+      }
+    }
+  }
+
+  Future<void> animateDot(Offset startOffset) async {
+    OverlayEntry entry = OverlayEntry(
+      builder: (context) {
+        double minTop = MediaQuery.of(context).padding.top + 52;
+        return AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return Positioned(
+              left: 79.5,
+              top: minTop +
+                  (startOffset.dy - minTop) * (1 - _animationController.value),
+              child: child,
+            );
+          },
+          child: Dot(),
+        );
+      },
+    );
+    Overlay.of(context).insert(entry);
+    await _animationController.forward(from: 0);
+    entry.remove();
+  }
+}
+
+class ItemFader extends StatefulWidget {
+  final Widget child;
+
+  const ItemFader({
+    Key key,
+    @required this.child,
+  }) : super(key: key);
+
+  @override
+  _ItemFaderState createState() => _ItemFaderState();
+}
+
+class _ItemFaderState extends State<ItemFader>
+    with SingleTickerProviderStateMixin {
+  //1 means its below, -1 means its above
+  int position = 1;
+  AnimationController _animationController;
+  Animation _animation;
+
+  void show() {
+    setState(() => position = 1);
+    _animationController.forward();
+  }
+
+  void hide() {
+    setState(() => position = -1);
+    _animationController.reverse();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, 64 * position * (1 - _animation.value)),
+          child: Opacity(
+            opacity: _animation.value,
+            child: child,
+          ),
+        );
+      },
+      child: widget.child,
+    );
+  }
+}
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const backgroundDecoration = BoxDecoration(
+  gradient: LinearGradient(
+    colors: [
+      Color.fromRGBO(76, 61, 243, 1),
+      Color.fromRGBO(28, 133, 103, 1),
+    ],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  ),
+);
